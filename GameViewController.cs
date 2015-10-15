9c27b26f2b8224df2b69e6a4e6640e0221b9e7d7
Glenn Stephens <glennstephens@Glenns-MacBook-Pro-2.local>
2015-10-15 10:37:03 -0500

    Added LongPress to Flag the mines
    Changed the Device Control to use Long Press for Flags. Much better for
    the device control

--- GameViewController.cs
+++ GameViewController.cs
@@ -16,10 +16,13 @@ namespace tvMinesweeper
 
 		public void FireAction(TVControllerAction item)
 		{
+			Console.WriteLine ("Remote Control Action: " + item);
 			scene.ProcessControllerAction (item);
 		}
 
 		GameScene scene;
+
+		DateTime LastFlagTime = DateTime.MinValue;
 
 		public override void ViewDidLoad ()
 		{
@@ -37,15 +40,23 @@ namespace tvMinesweeper
 			View.AddGestureRecognizer (new UISwipeGestureRecognizer (() => FireAction (TVControllerAction.Down)) {
 				Direction = UISwipeGestureRecognizerDirection.Down
 			});
-			View.AddGestureRecognizer (new UITapGestureRecognizer (() => FireAction (TVControllerAction.Tap))
+			View.AddGestureRecognizer (new UITapGestureRecognizer (() => {
+				// Trying to avoid the press after the long press
+				if ((DateTime.Now - LastFlagTime).TotalMilliseconds > 100)
+					FireAction (TVControllerAction.Tap);
+			})
 				{
 					AllowedPressTypes = new NSNumber[] { NSNumber.FromLong((nint)Convert.ToInt32 (UIPressType.Select)) },
 					AllowedTouchTypes = new NSNumber[] { NSNumber.FromLong((nint)Convert.ToInt32 (UITouchType.Direct)) },
 				});
-			View.AddGestureRecognizer (new UITapGestureRecognizer (() => FireAction (TVControllerAction.Flag))
+			View.AddGestureRecognizer (new UILongPressGestureRecognizer((lp) => {
+				// Only accept the first action as the UILongPressGestureRecognizer is a continuous recogniser
+				if (lp.State == UIGestureRecognizerState.Began)
 				{
-					AllowedPressTypes = new NSNumber[] { NSNumber.FromLong((nint)Convert.ToInt32 (UIPressType.PlayPause)) },
-				});
+					LastFlagTime = DateTime.Now;
+					FireAction (TVControllerAction.Flag);
+				}
+			}));
 						
 			var skView = (SKView)View;
 			skView.ShowsFPS = false;
